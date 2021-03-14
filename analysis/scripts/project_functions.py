774037325a8cbc17982e10ab1e6aec2b332e652a
Claire Costello <claire.cost.31@gmail.com>
2021-03-14 13:22:30 -0700

    chaining2

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,26 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def load_and_process(url_or_path_to_csv_file):
-
-    df1 = (
-          pd.read_csv(url_or_path_to_csv_file)
-          #.rename(...)
-          #.dropna(...)
-          # etc...
-      )
-
-    # Method Chain 2 (Create new columns, drop others, and do processing)
-
-    df2 = (
-          df1
-          .assign(...)
-      )
-
-    # Make sure to return the latest dataframe
-
-    return df2 
-
-
-def hello():
-    print("test")
+def load_and_process(path):
+    data = (pd.read_csv(path))
+    data.to_csv('../data/processed/data.csv')
+    return data
